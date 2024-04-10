f6ec08ce38c1eb372ccf9715f5cfdad4bbb16096
ruslanbaba <ruslan.babajanov3@gmail.com>
2024-04-10 04:48:52 +0000

    HW-2 files

---
+++ Homework2/main.tf
@@ -0,0 +1,42 @@
+
+#key
+resource "aws_key_pair" "Bastion-key" {
+  key_name   = "Bastion-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
+resource "aws_s3_bucket" "kaizen-ruslan" {
+  bucket = "kaizen-ruslan"
+}
+resource "aws_s3_bucket" "kaizen" {
+  bucket_prefix = "kaizen"
+}
+# Create manually two more buckets(S3)
+#terraform import aws_s3_
+#terraform import aws_s3_
+
+resource "aws_s3_bucket" "kaizen-ruslan09" {
+  bucket_prefix = "kaizen-ruslanb09"
+}
+resource "aws_s3_bucket" "kaizen-ruslan03" {
+  bucket_prefix = "kaizen-ruslanb03"
+}
+
+resource "aws_iam_user" "user" {
+  for_each = toset (["jenny", "rose", "lisa", "jisoo"]) 
+  name = each.key 
+}
+
+resource "aws_iam_group" "team" {
+  name = "blackpink"
+}
+
+resource "aws_iam_group_membership" "team" {
+  name = "blackpink"
+
+  users = [
+ for i in aws_iam_user.user : i.name
+  ]
+  group = aws_iam_group.team.name
+}
